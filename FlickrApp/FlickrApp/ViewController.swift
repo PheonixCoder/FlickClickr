9e782237d4fd1a1ff95e4551fa37300f0bb03294
PheonixCoder <76787135+PheonixCoder@users.noreply.github.com>
2021-11-24 12:12:41 +0200

    Add files via upload

---
+++ FlickrApp/FlickrApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrApp
+//
+//  Created by Hezi Cohen on 21/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
